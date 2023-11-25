595628d5dbf1b3f9fbd59fc55d690a52e882ed3d
Ribin K Thomas <95741045+Ribizz007@users.noreply.github.com>
2023-11-25 12:22:47 +0530

    Update BufferRegister.v

--- BufferRegister.v
+++ BufferRegister.v
@@ -1,21 +1,28 @@
 `timescale 1ns / 1ps
-parameter N = 1;
+
+// Define a Verilog module named BufferRegister
+// This module represents a buffer register with clock, clear, hold, input, and output signals
+// The width of the input and output is specified by the parameter N
 module BufferRegister(
+    input clk,           // Clock input
+    input clear,         // Clear signal input
+    input hold,          // Hold signal input
+    input wire [N-1:0] in, // Input data bus (width N)
+    output reg [N-1:0] out // Output data bus (width N)
+);
 
-    input clk,
-	input clear,
-	input hold,
-	input wire [N-1:0] in,
-	output reg [N-1:0] out);
-
-	//parameter N = 1;
-
-	always @(posedge clk) begin
-		if (clear)
-			out <= {N{1'b0}};
-		else if (hold)
-			out <= out;
-		else
-			out <= in;
-	end
+    // Always block triggered on the positive edge of the clock
+    always @(posedge clk) begin
+        // Check if the clear signal is active
+        if (clear)
+            // If clear is active, set the output to all zeros
+            out <= {N{1'b0}};
+        // Check if the hold signal is active
+        else if (hold)
+            // If hold is active, maintain the current output value
+            out <= out;
+        else
+            // If neither clear nor hold is active, update the output with the input value
+            out <= in;
+    end
 endmodule
